95e259e429b1fd6853d520c73780adeae40169e7
fbdevbr <fbdevbr@gmail.com>
2025-04-15 01:04:23 -0300

    adicionei o desafio pokemon, proposto no mapa de aventura

---
+++ desafio-pokemon.js
@@ -0,0 +1,15 @@
+let timePokemon = [ 
+    ["Poochyena", "Lv 2", "M", "13/13"],
+    ["Zigzagoon", "Lv 2", "F", "13/13"],
+    ["Dragonite", "Lv 5", "M", "25/25"],
+    ["Dragonite", "Lv 5", "F", "24/24"],
+    ["Dragonite", "Lv 5", "F", "24/24"],
+    ["Wurmple", "Lv 2", "M" , "7/14"]
+]
+
+console.log(timePokemon[0][0] + " " + "Cadastrado com sucesso!")
+console.log(timePokemon[1][0] + " " + "Cadastrado com sucesso!")
+console.log(timePokemon[2][0] + " " + "Cadastrado com sucesso!")
+console.log(timePokemon[3][0] + " " + "Cadastrado com sucesso!")
+console.log(timePokemon[4][0] + " " + "Cadastrado com sucesso!")
+console.log(timePokemon[5][0] + " " + "Cadastrado com sucesso!")
